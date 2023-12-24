90a7dc94c311892a7a9d42eb9d7d3894660db51b
Rahul <rahuldharan@hotmail.com>
2023-12-24 17:25:34 +0530

    Added feature to create TODO

--- src/app/api/todo/route.js
+++ src/app/api/todo/route.js
@@ -1,4 +1,61 @@
-export const dynamic = 'force-dynamic' // defaults to auto
+import { sql } from '@vercel/postgres'
+
+export const dynamic = 'force-dynamic'
 export async function GET(request) {
-    return Response.json({ data: "Hello, world!" })
+  return Response.json({ data: "Hello, world!" })
+}
+
+export async function PUT(request) {
+  try {
+    const { id, completed } = await request.json()
+
+    // validate request body
+    if (id == undefined || completed === undefined) {
+      return Response.json({ error: "id or completed missing" }, { status: 400 })
+    }
+
+    console.log(`${id} updated to ${completed}`)
+
+    try {
+      await sql`
+                UPDATE todos
+                SET completed = ${completed}
+                WHERE id = ${id}
+              `
+      return Response.json({ data: `${id} updated to ${completed}` }, { status: 200 })
+    } catch (error) {
+      return Response.json({ error: error.message }, { status: 500 })
+    }
+  } catch (error) {
+    console.log(error)
+    return Response.json({ error: "something went wrong" }, { status: 500 })
+  }
+}
+
+export async function POST(request) {
+  try {
+    // console.log(await request.json())
+    const {
+      wDate,
+      createdDate,
+      title,
+      category,
+      completed
+    } = await request.json()
+
+    await sql`
+      INSERT INTO todos (week_id, title, created_date, category, completed)
+      VALUES (
+          ${wDate}, 
+          ${title}, 
+          ${createdDate}, 
+          ${category}, 
+          ${completed}
+      )
+    `
+    return Response.json({ data: `${title} inserted` }, { status: 200 })
+  } catch (error) {
+    console.log(error)
+    return Response.json({ error: "something went wrong" }, { status: 500 })
+  }
 }
--- src/app/ui/add-todo-modal.js
+++ src/app/ui/add-todo-modal.js
@@ -1,30 +1,64 @@
 "use client"
-import React, { useRef } from 'react'
-import { createTodo } from "@/app/lib/actions"
+import React, { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 
 export function AddTodoModal() {
-  const formRef = useRef(null)
   const wDate = useSearchParams().get('wdate')
+  const [todo, useTodo] = useState({
+    createdDate: new Date(),
+    completed: false,
+    wDate,
+    title: '',
+    category: 'leverage'
+  })
+
+  const updateTodoWithIdNew = async () => {
+    try {
+      const response = await fetch('/api/todo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo),
+      })
+      const data = await response.json()
+      window.location.reload()
+    } catch (error) {
+      console.error('Failed to update todo', error)
+    }
+  }
+
+  const updateTodo = function (e) {
+    useTodo({ ...todo, [e.target.name]: e.target.value })
+  }
 
   return <>
     <dialog id="my_modal_1" className="modal">
       <div className="modal-box">
         <h3 className="font-bold text-lg">Enter Activity</h3>
 
-        <form ref={formRef} action={createTodo}>
-          <div className="modal-action">
-            <select className="select select-warning w-1/8" name="category">
-              <option value={"leverage"} defaultValue>L</option>
-              <option value={"neutral"}>N</option>
-              <option value={"overhead"}>O</option>
-            </select>
-            <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full" name="title" />
-            <input name="wDate" defaultValue={wDate} hidden />
+        <div className="modal-action">
+          <select
+            className="select select-warning w-1/8"
+            name="category"
+            onChange={(value) => updateTodo(value)}
+            value={todo.category}
+          >
+            <option value={"leverage"} defaultChecked>L</option>
+            <option value={"neutral"}>N</option>
+            <option value={"overhead"}>O</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered input-warning w-full"
+            name="title"
+            value={todo.name}
+            onChange={(value) => updateTodo(value)}
+          />
 
-            <button className="btn" type="submit" onClick={() => formRef.current && formRef.current.requestSubmit()}>Add</button>
-          </div>
-        </form>
+          <button className="btn" onClick={updateTodoWithIdNew} disabled={(todo.title == '') || (todo.category == '')}>Add</button>
+        </div>
 
       </div >
     </dialog >
